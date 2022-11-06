29f30225f13486e86aecb9437cd58c4a3048a908
root <99701537@qq.com>
2022-11-06 13:56:40 +0800

    添加了一个输出

--- src/main/java/com/northcastle/HelloWorld.java
+++ src/main/java/com/northcastle/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!!");
         System.out.println("This is Maven!");
+        System.out.println("我修改了！！！");
     }
 }
